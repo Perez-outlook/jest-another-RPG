1999a1eacdc0b626b8077658cfe067a07d2ad585
dianepj12@gmail.com <dianepj12@gmail.com>
2022-01-16 23:23:34 -0600

    understanding .prototype

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,6 +3,9 @@ const Player = require('../lib/Player');
 
 Test('creates a player object',() =>{
     const player = new Player('Dave');
+
+    player.getStats();
+    player.getInventory();
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
@@ -10,4 +13,22 @@ Test('creates a player object',() =>{
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+test("get player's stats as an object", () =>{
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () =>{
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    Player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
